2663b3bc61576e66ddcd41fd3b21ca4ff31bba36
Samson <sams1307wolde@gmail.com>
2026-09-12 12:48:35 +0300

    Update formatter to handle MongoDB default time

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,10 +1,15 @@
-export const dateLabel = (date: string) =>
-  new Intl.DateTimeFormat("en-US", {
+export const dateLabel = (date: string) => {
+  const dateOnly = date.split("T")[0];
+  const d = new Date(`${dateOnly}T12:00:00`);
+
+  if (isNaN(d.getTime())) return "";
+
+  return new Intl.DateTimeFormat("en-US", {
     month: "short",
     day: "numeric",
     year: "numeric",
-  }).format(new Date(`${date}T12:00:00`));
-
+  }).format(d);
+};
 export const money = (amount: number) =>
   new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(
     amount
