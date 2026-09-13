d8ec0acfde837f9c1a0ddab16862e9fc71b34ad9
Samson <sams1307wolde@gmail.com>
2026-09-13 17:47:24 +0300

    Remove custom font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ExpenseProvider } from "@/context/ExpenseContext";
 
 export const metadata: Metadata = {
   title: "Pennywise | Expense management, made simple",
@@ -21,11 +11,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+    <html lang="en" className={`h-full antialiased`}>
       <body className="min-h-full flex flex-col">
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <ExpenseProvider>{children}</ExpenseProvider>
+        </AuthProvider>
       </body>
     </html>
   );
